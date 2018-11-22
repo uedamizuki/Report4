0a0c5f9b5e008513ef8d0ebb142da83d8955ef11
Ueda Mizuki <e185719@ie.u-ryukyu.ac.jp>
2018-11-22 22:53:42 +0900

    2nd commit

--- src/Exception.java
+++ src/Exception.java
@@ -1,7 +1,13 @@
 public class Exception {
-    public static void main (String[] args){
-        String str = null;
+    public static void main(String[] args) {
+        try {
+            String str = null;
 
-        System.out.println(str.length());
+            System.out.println(str.length());
+
+        } catch (NullPointerException e) {
+            System.out.println(e + "のエラーです。");
+            System.out.println(e.getMessage());
+        }
     }
 }
